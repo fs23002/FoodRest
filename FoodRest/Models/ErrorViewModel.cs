4d5bd35f68d02b86e552b4eb5585cba9dac664fe
fs23002 <fs23002@itcha.edu.sv>
2024-07-30 11:03:15 -0600

    first commit

---
+++ FoodRest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodRest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
